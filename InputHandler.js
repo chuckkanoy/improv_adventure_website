530d311d768b20562ddd3768f01e75fe0fd0b334
unknown <ch.kanoy@gmail.com>
2020-03-25 12:04:09 -0500

    began process of creating graph

--- InputHandler.js
+++ InputHandler.js
@@ -9,6 +9,7 @@ var pres = "",
 
 //take in information and display it
 function gatherInfo() {
+    player = document.getElementById("player").value;
     pres = document.getElementById("president").value;
     vp = document.getElementById("vp").value;
     treasurer = document.getElementById("treasurer").value;
@@ -17,6 +18,7 @@ function gatherInfo() {
     pc = document.getElementById("pc").value;
     em = document.getElementById("em").value;
 
+    document.cookie = "player=" + player + "; path=/";
     document.cookie = "pres=" + pres + "; path=/";
     document.cookie = "vp=" + vp + "; path=/";
     document.cookie = "treasurer=" + treasurer + "; path=/";
@@ -31,7 +33,7 @@ function gatherInfo() {
 
 //verify the information given
 function verify() {
-    var verified = confirm("Is the following information correct?\nPresident: " + pres + "\nVice President: " + vp + "\nTreasurer: " + treasurer + "\nSecretary: " + secretary +
+    var verified = confirm("Is the following information correct?\nYour name: " + player + "\nPresident: " + pres + "\nVice President: " + vp + "\nTreasurer: " + treasurer + "\nSecretary: " + secretary +
         "\nWeb Master: " + web + "\nPublicity Coordinator: " + pc + "\nEquipment Manager: " + em);
     return verified;
 }
